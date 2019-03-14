8496ed9a09227577463a6a703bf2cee21b894540
IrenaRistova <36274479+IrenaRistova@users.noreply.github.com>
2019-03-14 22:06:42 +0100

    Add files via upload

---
+++ cpp-chat-server/Server.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 14-Mar-19.
+//
+
+#ifndef CPP_CHAT_SERVER_SERVER_H
+#define CPP_CHAT_SERVER_SERVER_H
+
+#endif //CPP_CHAT_SERVER_SERVER_H
---
+++ cpp-chat-server/build/CMakeFiles/cpp_chat_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_chat_server.dir/main.cpp.obj"
+  "cpp_chat_server.pdb"
+  "cpp_chat_server.exe"
+  "cpp_chat_server.exe.manifest"
+  "libcpp_chat_server.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_chat_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
